152548fd31e3517c4a163aab2528d83752ad812c
Jeremiah Gelb <jmgelb@ncsu.edu>
2020-11-10 09:22:04 -0500

    Solver: clean up and fix includes

--- src/Solver/Solver.cpp
+++ src/Solver/Solver.cpp
@@ -1,8 +1,8 @@
 #include "Solver.hpp"
 #include <deque>
-#include <iostream>
 #include <vector>
 #include <utility>
+#include <algorithm>
 namespace {
 
 struct SolvePosition {
@@ -27,25 +27,19 @@ Solver::Solver(std::shared_ptr<Board> board,
 : allow_cycles_(allow_cycles)
 , trie_(trie)
 , board_(board) {
-    // std::cout << "Enter Solver Constructor" << std::endl;
     using WorkList = std::deque<SolvePosition>;
     const auto add_next_solve_positions_to_work_list = [this] (const SolvePosition & position,
                                                                WorkList & work_list) {
         const auto & trie_node = position.trie_node_;
         const auto & board_node = position.board_node_;
-        // std::cout << "----------------------------------" << std::endl;
-        // std::cout << "investigating trie_node: " << trie_node->letter()
-        //           << " board node: " << board_node->letter() << std::endl;
 
         const auto & board_edges_map = board_node->edges();
         for (const auto & [trie_letter, trie_edges] : trie_node->edges()) {
             try {
-                // std::cout << "trying trie_letter: " << trie_letter << std::endl;
                 const auto & board_edges = board_edges_map.at(trie_letter);
-                // std::cout << "board had: " << board_edges.size() << std::endl;
                 const auto & trie_edge = *trie_edges.begin();
-
                 const auto & board_path = position.board_path_;
+
                 for (const auto & board_edge : board_edges) {
                     if (!allow_cycles_) {
                         if (std::find(board_path.begin(),
@@ -57,23 +51,19 @@ Solver::Solver(std::shared_ptr<Board> board,
                     work_list.emplace_back(SolvePosition(trie_edge, board_edge, std::move(board_path)));
                 }
             } catch (std::out_of_range & e) {
-                // std::cout << "board didn't have it" << std::endl;
                 continue;
             }
         }
-        // std::cout << "------------------------------" << std::endl;
     };
 
     WorkList work_list;
     const auto trie_head = trie->head();
-    // std::cout << "Filling from board" << std::endl;
     for (const auto & [letter, letter_node_set] : board->tiles()) {
         for (const auto & board_edge : letter_node_set) {
             work_list.emplace_back(SolvePosition(trie_head, board_edge, {}));
         }
     }
 
-    // std::cout << "Starting Solve" << std::endl;
     while (!work_list.empty()) {
         const auto & position = work_list.front();
         words_.insert(position.trie_node_->name());
